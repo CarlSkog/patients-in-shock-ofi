ff184c31f42474bdbf20048ceebac3806632b452
CarlSkog <carls@noacs.io>
2024-09-29 18:00:51 +0200

    ändrade rätt i snuttarna så att det är faktiska negativa tal
    la till nya intro

--- Sparade snuttar.R
+++ Sparade snuttar.R
@@ -8,7 +8,11 @@ function(x){
 BE <- convert_number(data$swetrau_scrambled$ed_be_art)
 
 BEclass1 <- swetrau_scrambled[BE<0 & BE>-2, ]
-BEclass2 <- swetrau_scrambled[BE<-2 & BE>-6, ]
-BEclass3 <- swetrau_scrambled[BE<-6 & BE>-10, ]
-BEclass4 <- swetrau_scrambled[BE<-10, ]
+BEclass2 <- swetrau_scrambled[BE<(-2) & BE>(-6), ]
+BEclass3 <- swetrau_scrambled[BE<(-6) & BE>-10, ]
+BEclass4 <- swetrau_scrambled[BE<(-10), ]
 
+BEclass1num <- convert_number(BEclass1$ed_be_art)
+BEclass2num <- convert_number(BEclass2$ed_be_art)
+BEclass3num <- convert_number(BEclass3$ed_be_art)
+BEclass4num <- convert_number(BEclass4$ed_be_art)
