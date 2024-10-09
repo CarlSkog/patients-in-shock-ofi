c979bbda74c4ecf4f77e4c2c8a32765c74f6ac14
CarlSkog <carls@noacs.io>
2024-10-09 18:13:36 +0200

    chi sqr testat både ofi och grad av shock samt ofi med shock

--- main.R
+++ main.R
@@ -115,8 +115,24 @@ BEc3N <- filter(BEclass3, ofi == "No")
 BEc4Y <- filter(BEclass4, ofi == "Yes")
 BEc4N <- filter(BEclass4, ofi == "No")
 
-tab <- as.table(rbind(c(sum(length(convert_number(BEc1Y$ed_be_art))), sum(length(convert_number(BEc2Y$ed_be_art))), sum(length(convert_number(BEc3Y$ed_be_art))), sum(length(convert_number(BEc4Y$ed_be_art)))), 
+tabell <- as.table(rbind(c(sum(length(convert_number(BEc1Y$ed_be_art))), sum(length(convert_number(BEc2Y$ed_be_art))), sum(length(convert_number(BEc3Y$ed_be_art))), sum(length(convert_number(BEc4Y$ed_be_art)))), 
                       c(sum(length(convert_number(BEc1N$ed_be_art))), sum(length(convert_number(BEc2N$ed_be_art))), sum(length(convert_number(BEc3N$ed_be_art))), sum(length(convert_number(BEc4N$ed_be_art))))))
-dimnames(tab) <- list(OFI = c("Yes", "No"),
+dimnames(tabell) <- list(OFI = c("Yes", "No"),
                       Shock_class = c("Class 1", "Class 2", "Class 3", "Class 4"))
-tab
+tabell
+chisq.test(tabell)
+
+
+BEclass0 <- study.sampleBE[BE<0, ]
+BEclass999 <- study.sampleBE[BE>= 0, ]
+BEc0Y <- filter(BEclass0, ofi == "Yes")
+BEc0N <- filter(BEclass0, ofi == "No")
+BEc999Y <- filter(BEclass999, ofi == "Yes")
+BEc999N <- filter(BEclass999, ofi == "No")
+
+tabell <- as.table(rbind(c(sum(length(convert_number(BEc0Y$ed_be_art))), sum(length(convert_number(BEc999Y$ed_be_art)))),
+                         c(sum(length(convert_number(BEc0N$ed_be_art))), sum(length(convert_number(BEc999N$ed_be_art))))))
+dimnames(tabell) <- list(OFI = c("Yes", "No"),
+                         Shock = c("Yes shock", "no shock"))
+tabell
+chisq.test(tabell)
